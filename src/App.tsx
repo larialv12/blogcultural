b14ac3a4808d44d84e00ce2eb8a60534b5e8141f
larissa.siqueira <larialves200157@gmail.com>
2026-03-25 23:48:26 -0300

    ajuste

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Home from './pages/Home/Home'
+import Menu from './pages/Menu/Menu'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Menu/>
+      <Home/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Menu/Menu.tsx
@@ -0,0 +1,23 @@
+
+function Menu() {
+  return (
+<div className='w-full flex justify-center py-4
+                bg-indigo-900 text-white'>
+
+    <div className="container flex justify-between text-lg mx-8">
+        Blog Cultural
+
+        <div className='flex  justify-items-center'>
+
+          
+            Eventos
+            Artistas
+            Locais
+            Noticias
+            
+        </div>
+    </div>
+</div>  )
+}
+
+export default Menu
